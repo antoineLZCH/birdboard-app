4f9f781e5a32d8fcc5d9b3f5254ebb06c65e52f4
Antoine LZCH <antoine@agence-osez.com>
2020-04-12 18:15:47 +0200

    project requires owner, make:auth

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -20,8 +20,8 @@ class ProjectsController extends Controller
 
         $attributes['owner_id'] = auth()->id();
 
+        auth()->user()->projects()->create($attributes);
 
-        Project::create($attributes);
         return redirect('/projects');
     }
 
--- tests/Feature/ProjectTest.php
+++ tests/Feature/ProjectTest.php
@@ -11,8 +11,16 @@ class ProjectTest extends TestCase
     use WithFaker, RefreshDatabase;
 
     /** @test */
+    public function only_authenticated_users_can_create_projects() {
+        $attributes = factory('App\Project')->raw();
+        $this->post('/projects', $attributes)->assertRedirect('login');
+    }
+
+    /** @test */
     public function a_user_can_create_a_project() {
         $this->withoutExceptionHandling();
+
+        $this->actingAs(factory('App\User')->create());
 
         $attributes = [
             'title' => $this->faker->sentence,
@@ -39,19 +47,15 @@ class ProjectTest extends TestCase
 
     /** @test */
     public function a_project_requires_a_title() {
+        $this->actingAs(factory('App\User')->create());
         $attributes = factory('App\Project')->raw(['title' => '']);
         $this->post('/projects', $attributes)->assertSessionHasErrors('title');
     }
 
     /** @test */
     public function a_project_requires_a_description() {
+        $this->actingAs(factory('App\User')->create());
         $attributes = factory('App\Project')->raw(['description' => '']);
         $this->post('/projects', $attributes)->assertSessionHasErrors('description');
-    }
-
-    /** @test */
-    public function a_project_requires_an_owner() {
-        $attributes = factory('App\Project')->raw(['owner_id' => null]);
-        $this->post('/projects', $attributes)->assertSessionHasErrors('owner_id');
     }
 }
